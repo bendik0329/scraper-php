9e28d7b98ab2b8d45c3ff6686603273e587d6dca
bendik0329 <razvanbendik0329@gmail.com>
2023-08-21 22:37:01 +0300

    update scraping

--- src/index.php
+++ src/index.php
@@ -6,51 +6,95 @@ require_once  __DIR__ . '/utils/scraping.php';
 
 use voku\helper\HtmlDomParser;
 
-// initializing the cURL request
+print_r("Hello Everybody");
+exit();
+// foreach (STATE_LIST as $key => $value) {
+//   $state = strtolower($key);
+  
+//   $filterState = array(
+//     "fsba" => ["value" => false],
+//     "fsbo" => ["value" => false],
+//     "nc" => ["value" => false],
+//     "cmsn" => ["value" => false],
+//     "auc" => ["value" => false],
+//     "sort" => ["value" => "globalrelevanceex"],
+//     "beds" => ["min" => 1],
+//     "baths" => ["min" => 1],
+//     "sqft" => ["max" => 500],
+//   );
+//   $query = array(
+//     "isMapVisible"=> true,
+//     "filterState" => $filterState,
+//     "isListVisible"=> true,
+//     "mapZoom"=> 6,
+//     "usersSearchTerm"=> "CA"
+//   );
+//   $queryString = json_encode($query);
+//   $searchQueryState = urlencode($queryString);
+
+//   $url = "https://www.zillow.com/$state/foreclosures/?searchQueryState=$searchQueryState";
+//   print_r($url . "\n");
+// }
+// exit();
+$url = 'https://www.zillow.com/wy/foreclosures/';
 $curl = curl_init();
-// setting the URL to reach with a GET HTTP request
-curl_setopt($curl, CURLOPT_URL, 'https://www.zillow.com/homes/for_sale/fore_lt/?searchQueryState=%7B%22mapBounds%22%3A%7B%22north%22%3A42.009517%2C%22east%22%3A-114.131253%2C%22south%22%3A32.528832%2C%22west%22%3A-124.482045%7D%2C%22mapZoom%22%3A6%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22ah%22%3A%7B%22value%22%3Atrue%7D%2C%22sort%22%3A%7B%22value%22%3A%22globalrelevanceex%22%7D%2C%22auc%22%3A%7B%22value%22%3Afalse%7D%2C%22nc%22%3A%7B%22value%22%3Afalse%7D%2C%22fsbo%22%3A%7B%22value%22%3Afalse%7D%2C%22cmsn%22%3A%7B%22value%22%3Afalse%7D%2C%22fsba%22%3A%7B%22value%22%3Afalse%7D%2C%22sche%22%3A%7B%22value%22%3Afalse%7D%2C%22schm%22%3A%7B%22value%22%3Afalse%7D%2C%22schh%22%3A%7B%22value%22%3Afalse%7D%2C%22schp%22%3A%7B%22value%22%3Afalse%7D%2C%22schr%22%3A%7B%22value%22%3Afalse%7D%2C%22schc%22%3A%7B%22value%22%3Afalse%7D%2C%22schu%22%3A%7B%22value%22%3Afalse%7D%7D%2C%22isListVisible%22%3Atrue%7D');
-// to make the cURL request follow eventual redirects
-// and reach the final page of interest
-curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
-// to get the data returned by the cURL request as a string
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-// setting the User-Agent header
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_PROXY, 'http://60cb4030eb5826662d404f6cb6bb10040a3f775a:@proxy.zenrows.com:8001');
+curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'GET');
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
 curl_setopt($curl, CURLOPT_USERAGENT, USER_AGENT);
-// executing the cURL request and
-// get the HTML of the page as a string
+curl_setopt($curl, CURLOPT_HTTPHEADER, [
+  'Referrer: https://www.google.com',
+]);
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
 $html = curl_exec($curl);
-// releasing the cURL resources
 curl_close($curl);
 
-print_r($html);exit();
-// initializing HtmlDomParser
 $htmlDomParser = HtmlDomParser::str_get_html($html);
 
-// retrieving the HTML pagination elements with
-// the ".page-numbers a" CSS selector
-$paginationElements = $htmlDomParser->find(".page-numbers a");
-$paginationLinks = [];
-foreach ($paginationElements as $paginationElement) {
-    // populating the paginationLinks set with the URL
-    // extracted from the href attribute of HTML pagination element
-    $paginationLink = $paginationElement->getAttribute("href");
-    // avoiding duplicates in the list of URLs
-    if (!in_array($paginationLink, $paginationLinks)) {
-        $paginationLinks[] = $paginationLink;
-    }
+$resultCountText = $htmlDomParser->findOne(".search-page-list-header .search-subtitle span.result-count")->text;
+$resultCount = 0;
+if (preg_match('/(\d+)/', $resultCountText, $matches)) {
+    $resultCount = $matches[1];
 }
 
-// removing all non-numeric characters in the last element of
-// the $paginationLinks array to retrieve the highest pagination number
-$highestPaginationNumber = preg_replace("/\D/", '', end($paginationLinks));
-
-$productDataList = array();
-// iterate over all "/shop/pages/X" pages and retrieve all product data
-for ($paginationNumber = 1; $paginationNumber <= $highestPaginationNumber; $paginationNumber++) {
-    $productDataList = array_merge($productDataList, scrapeShopPage($paginationNumber));
+$propertyCountPerPage = $htmlDomParser->findOne("#search-page-list-container .result-list-container ul.photo-cards")->childNodes->length;
+print_r($propertyCountPerPage);
+exit();
+if ($resultCount <= $propertyCountPerPage) {
+  print_r("Hey bro");
 }
+exit();
 
-echo json_encode($productDataList);
+$i = 0;
+foreach($propertyList as $propertyItem) {
+  print_r("index:" . $i);
+  $i++;
+}
+exit();
 
-// writing the data scraped to a database/file
+$propertyListArray = get_object_vars($propertyList);
+
+if ($resultCount <= count($propertyListArray)) {
+
+}
+print_r($propertyListArray);
+exit();
+
+// $i = 0;
+// foreach($propertyList as $propertyItem) {
+//   print_r("index:" . $i);
+//   $i++;
+// }
+// exit();
+// $highestPaginationNumber = preg_replace("/\D/", '', end($count));
+
+// print_r($highestPaginationNumber);
+// exit();
+// $result = array();
+// $result = array_merge($result, scrateForeclosure());
+
+// echo json_encode($result);
+// exit();
--- src/utils/scraping.php
+++ src/utils/scraping.php
@@ -4,41 +4,113 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 
 use voku\helper\HtmlDomParser;
 
-function scrapeShopPage($paginationNumber) {
-    $productDataList = array();
+function scrapeShopPage($paginationNumber)
+{
+  $productDataList = array();
 
-    $curl = curl_init();
-    curl_setopt($curl, CURLOPT_URL, "https://scrapeme.live/shop/page/$paginationNumber");
-    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
-    curl_setopt($curl, CURLOPT_USERAGENT, USER_AGENT);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    $pageHtml = curl_exec($curl);
-    curl_close($curl);
+  $curl = curl_init();
+  curl_setopt($curl, CURLOPT_URL, "https://scrapeme.live/shop/page/$paginationNumber");
+  curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+  curl_setopt($curl, CURLOPT_USERAGENT, USER_AGENT);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+  $pageHtml = curl_exec($curl);
+  curl_close($curl);
 
-    $paginationHtmlDomParser = HtmlDomParser::str_get_html($pageHtml);
+  $paginationHtmlDomParser = HtmlDomParser::str_get_html($pageHtml);
 
-    // retrieving the list of products on the page
-    $productElements = $paginationHtmlDomParser->find("li.product");
+  // retrieving the list of products on the page
+  $productElements = $paginationHtmlDomParser->find("li.product");
 
-    foreach ($productElements as $productElement) {
-        $productDataList[] = scrapeProduct($productElement);
-    }
+  foreach ($productElements as $productElement) {
+    $productDataList[] = scrapeProduct($productElement);
+  }
 
-    return $productDataList;
+  return $productDataList;
 }
 
-function scrapeProduct($productElement) {
-    // extracting the product data
-    $url = $productElement->findOne("a")->getAttribute("href");
-    $image = $productElement->findOne("img")->getAttribute("src");
-    $name = $productElement->findOne("h2")->text;
-    $price = $productElement->findOne(".price span")->text;
+function scrapeProduct($productElement)
+{
+  // extracting the product data
+  $url = $productElement->findOne("a")->getAttribute("href");
+  $image = $productElement->findOne("img")->getAttribute("src");
+  $name = $productElement->findOne("h2")->text;
+  $price = $productElement->findOne(".price span")->text;
 
-    // transforming the product data in an associative array
-    return array(
-        "url" => $url,
-        "image" => $image,
-        "name" => $name,
-        "price" => $price
-    );
+  // transforming the product data in an associative array
+  return array(
+    "url" => $url,
+    "image" => $image,
+    "name" => $name,
+    "price" => $price
+  );
+}
+
+function scrateForeclosure()
+{
+  $dataList = array();
+
+  $url = 'https://www.zillow.com/wy/foreclosures/';
+  $curl = curl_init();
+  curl_setopt($curl, CURLOPT_URL, $url);
+  curl_setopt($curl, CURLOPT_PROXY, 'http://60cb4030eb5826662d404f6cb6bb10040a3f775a:@proxy.zenrows.com:8001');
+  curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'GET');
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
+  curl_setopt($curl, CURLOPT_USERAGENT, USER_AGENT);
+  curl_setopt($curl, CURLOPT_HTTPHEADER, [
+    'Referrer: https://www.google.com',
+  ]);
+  curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+  curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+  $pageHtml = curl_exec($curl);
+  curl_close($curl);
+
+  $paginationHtmlDomParser = HtmlDomParser::str_get_html($pageHtml);
+
+  // retrieving the list of products on the page
+  $productElements = $paginationHtmlDomParser->find("#search-page-list-container ul li article.property-card div");
+
+  foreach ($productElements as $productElement) {
+    $dataList[] = scrateItem($productElement);
+  }
+
+  return $dataList;
+}
+
+function scrateItem($element)
+{
+  // get url
+  $swipeFirstElement = $element->findone("#swipeable")->firstChild();
+  $url = $swipeFirstElement->findOne("a")->getAttribute("href");
+
+  // get image list
+  $imgList = [];
+  $swipeElements = $element->find("#swipeable div");
+  foreach($swipeElements as $swipeElement) {
+    $imgList[] = $swipeElement->findOne("a div picture img")->getAttribute("src");
+  }
+
+  $propertyDataElement = $element->findOne("div.property-card-data");
+
+  return array(
+    "url" => $url,
+    "images" => $imgList,
+  );
+
+  // $url = $element->findOne('//*[@id="swipeable"]/div[1]/a')->getAttribute("href");
+  // $bed = $element->findOne('//*[@id="zpid_17500338"]/div/div[1]/div[3]/ul/li[1]/b')->text;
+  // $bath = $element->findOne('//*[@id="zpid_17500338"]/div/div[1]/div[3]/ul/li[2]/b')->text;
+  // $sqft = $element->findOne('//*[@id="zpid_17500338"]/div/div[1]/div[3]/ul/li[3]/b')->text;
+  // $price = $element->findOne('//*[@id="zpid_21109934"]/div/div[1]/div[2]/div/span')->text;
+  // $address = $element->findOne('//*[@id="zpid_17500338"]/div/div[1]/a/address')->text;
+
+  // // transforming the product data in an associative array
+  // return array(
+  //   "url" => $url,
+  //   "bed" => $bed,
+  //   "bath" => $bath,
+  //   "sqft" => $sqft,
+  //   "price" => $price,
+  //   "address" => $address,
+  // );
 }
